632fe1953a4f066b22afaac7a02226ddd45d597f
Mollalign <molledan69@gmail.com>
2026-02-02 14:20:58 +0300

    refactor: simplify theme handling logic in RootLayout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -65,10 +65,6 @@ export default function RootLayout({
                 const theme = localStorage.getItem('theme');
                 if (theme === 'light') {
                   document.documentElement.classList.add('light');
-                } else if (theme === 'dark') {
-                  document.documentElement.classList.add('dark');
-                } else if (window.matchMedia('(prefers-color-scheme: light)').matches) {
-                  document.documentElement.classList.add('light');
                 } else {
                   document.documentElement.classList.add('dark');
                 }
